14d916efafb4b6b9d3c43df9b266673d95a13f38
Arthur Uzulin <fuj1nman@hotmail.com>
2019-09-08 11:20:27 +1000

    Added recipes and tags for metals

--- src/main/java/slimeknights/tmechworks/common/blocks/MetalBlock.java
+++ src/main/java/slimeknights/tmechworks/common/blocks/MetalBlock.java
@@ -1,6 +1,7 @@
 package slimeknights.tmechworks.common.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
 
@@ -8,6 +9,6 @@ public class MetalBlock extends Block
 {
     public MetalBlock()
     {
-        super(Properties.create(Material.IRON).hardnessAndResistance(5F, 10F).harvestTool(ToolType.PICKAXE).harvestLevel(-1));
+        super(Properties.create(Material.IRON).hardnessAndResistance(5F, 10F).harvestTool(ToolType.PICKAXE).harvestLevel(-1).sound(SoundType.METAL));
     }
 }
